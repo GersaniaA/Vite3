896a95990723227dc9f916b240496d6868e14516
gersania <agersania@gmail.com>
2025-03-17 10:31:59 -0600

    subida

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./database/authcontext";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import Login from './views/Login'
+import Encabezado from "./components/Encabezado";
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias"; //Importación de Categorias
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Encabezado />
+            <main>
+              <Routes>
+                
+                <Route path="/" element={<Login />} />
+                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+                <Route path="/categorias" element={<ProtectedRoute element={<Categorias />} />}/> //Ruta de Categorias protegida
+
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AuthProvider>
+    </>
+  )
+}
+
+export default App
---
+++ src/database/firebaseconfig.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Asegúrate de importar esto
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdGyaXkvsdzGpAlGupZf_9NFx4qNqy9hM",
+  authDomain: "vite-8af48.firebaseapp.com",
+  projectId: "vite-8af48",
+  storageBucket: "vite-8af48.firebasestorage.app",
+  messagingSenderId: "796621177176",
+  appId: "1:796621177176:web:3f3ac92ec7dc0e5757652d",
+  measurementId: "G-MNVM77WF6K"
+};
+
+
+const appfirebase = initializeApp(firebaseConfig);
+
+const db = getFirestore(appfirebase);
+
+const auth = getAuth(appfirebase);
+
+export {appfirebase, db, auth};
