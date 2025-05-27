a1410545628f98a849613b09e2707e211cd12561
gersania <agersania@gmail.com>
2025-05-26 19:03:37 -0600

    Generacion de PDF y Excel

--- src/views/Productos.jsx
+++ src/views/Productos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Col } from "react-bootstrap";
 import { db } from "../database/firebaseconfig";
 import {
   collection,
@@ -16,6 +16,11 @@ import ModalEdicionProducto from "../components/productos/ModalEdicionProducto";
 import ModalEliminacionProducto from "../components/productos/ModalEliminacionProducto";
 import CuadroBusquedas from "../components/busquedas/CuadroBusquedas";
 import ModalQR from "../components/qr/ModalQr";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+
 
 const Productos = () => {
     const [showQRModal, setShowQRModal] = useState(false);
@@ -358,60 +363,202 @@ const Productos = () => {
   };
 }, []);
 
-  return (
-    <Container className="mt-5">
-      <br />
-      <h4>Gestión de Productos</h4>
-      <Button className="mb-3" onClick={() => setShowModal(true)}>
-        Agregar producto
-      </Button>
-      <CuadroBusquedas
-        searchText={searchText}
-        handleSearchChange={handleSearchChange}
-      />
-      <TablaProductos
-        openEditModal={openEditModal}
-        openDeleteModal={openDeleteModal}
-        handleCopy={handleCopy}
-        openQRModal={openQRModal}
-        productos={paginatedProductos}
-        totalItems={productosFiltrados.length}
-        itemsPerPage={itemsPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-      <ModalRegistroProducto
-        showModal={showModal}
-        setShowModal={setShowModal}
-        nuevoProducto={nuevoProducto}
-        handleInputChange={handleInputChange}
-        handleImageChange={handleImageChange}
-        handleAddProducto={handleAddProducto}
-        categorias={categorias}
-      />
-      <ModalEdicionProducto
-        showEditModal={showEditModal}
-        setShowEditModal={setShowEditModal}
-        productoEditado={productoEditado}
-        handleEditInputChange={handleEditInputChange}
-        handleEditImageChange={handleEditImageChange}
-        handleEditProducto={handleEditProducto}
-        categorias={categorias}
-      />
-      <ModalEliminacionProducto
-        showDeleteModal={showDeleteModal}
-        setShowDeleteModal={setShowDeleteModal}
-        handleDeleteProducto={handleDeleteProducto}
-        
-      />
+const generarPDFProductos = () => {
+    const doc = new jsPDF();
+    doc.setFillColor(28, 41, 51);
+    doc.rect(0, 0, 220, 30, 'F');
+    doc.setTextColor(255, 255, 255);
+    doc.setFontSize(28);
+    doc.text("Lista de Productos", doc.internal.pageSize.getWidth() / 2, 18, {align: "center"});
 
-      <ModalQR
+    const columnas = ["#", "Nombre", "Precio", "Categoria"];
+    const filas = productosFiltrados.map((p, i) => [
+      i + 1,
+      p.nombre,
+      `C$ ${p.precio}`,
+      p.categoria
+    ]);
+
+    autoTable(doc, {
+      head: [columnas],
+      body: filas,
+      startY: 40,
+      theme: "grid",
+      styles: { fontSize: 10, cellPadding: 2 },
+      margin: { top: 20, left: 14, right: 14 },
+      tableWidth: "auto",
+      didDrawPage: (data) => {
+        const pageHeight = doc.internal.pageSize.getHeight();
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const pageNumber = doc.internal.getNumberOfPages();
+        doc.setFontSize(10);
+        doc.setTextColor(0);
+        doc.text(`Página ${pageNumber}`, pageWidth / 2, pageHeight - 10, { align: "center" });
+      },
+    });
+
+    const fecha = new Date();
+    const nombreArchivo = `productos_${fecha.getDate().toString().padStart(2, '0')}${(fecha.getMonth()+1).toString().padStart(2, '0')}${fecha.getFullYear()}.pdf`;
+    doc.save(nombreArchivo);
+  };
+
+
+     // Generar PDF para un producto específico
+  const generarPDFDetalleProducto = (producto) => {
+    const pdf = new jsPDF();
+
+    // Encabezado
+    pdf.setFillColor(28, 41, 51);
+    pdf.rect(0, 0, 220, 30, 'F');
+    pdf.setTextColor(255, 255, 255);
+    pdf.setFontSize(22);
+    pdf.text(producto.nombre, pdf.internal.pageSize.getWidth() / 2, 18, { align: "center" });
+
+    // Imagen
+    if (producto.imagen) {
+      const propiedadesImagen = pdf.getImageProperties(producto.imagen);
+      const anchoPagina = pdf.internal.pageSize.getWidth();
+      const anchoImagen = 60;
+      const altoImagen = (propiedadesImagen.height * anchoImagen) / propiedadesImagen.width;
+      const posicionX = (anchoPagina - anchoImagen) / 2;
+      pdf.addImage(producto.imagen, 'JPEG', posicionX, 40, anchoImagen, altoImagen);
+
+      const posicionY = 40 + altoImagen + 10;
+      pdf.setTextColor(0, 0, 0);
+      pdf.setFontSize(14);
+      pdf.text(`Precio: C$ ${producto.precio}`, anchoPagina / 2, posicionY, { align: "center" });
+      pdf.text(`Categoría: ${producto.categoria}`, anchoPagina / 2, posicionY + 10, { align: "center" });
+    } else {
+      const anchoPagina = pdf.internal.pageSize.getWidth();
+      pdf.setTextColor(0, 0, 0);
+      pdf.setFontSize(14);
+      pdf.text(`Precio: C$ ${producto.precio}`, anchoPagina / 2, 50, { align: "center" });
+      pdf.text(`Categoría: ${producto.categoria}`, anchoPagina / 2, 60, { align: "center" });
+    }
+
+    pdf.save(`${producto.nombre}.pdf`);
+  };
+
+
+  const exportarExcelProductos = () => {
+      const datos = productosFiltrados.map( (producto, index) => ({
+    "#": index + 1,
+    Nombre: producto.nombre,
+    Precio: parseFloat(producto.precio),
+    Categoría: producto.categoria,
+    }));
+
+    // Crear hoja y libro Excel
+    const hoja = XLSX.utils.json_to_sheet(datos);
+    const libro = XLSX.utils.book_new( );
+    XLSX. utils.book_append_sheet(libro, hoja, 'Productos' );
+    // Crear el archivo binario
+    const excelBuffer = XLSX.write(libro, { bookType: 'xlsx', type: 'array' });
+
+    // Guardar el Excel con un nombre basado en la fecha actual
+      const fecha = new Date( );
+      const dia = String(fecha.getDate()).padStart(2, '0');
+      const mes = String(fecha. getMonth( ) + 1) . padStart(2, '0' );
+      const anio = fecha.getFullYear( );
+      const nombreArchivo = `Productos_${dia}${mes}${anio}.xlsx` ;
+      // Guardar archivo
+      const blob = new Blob( [excelBuffer], { type: 'application/octet-stream' });
+      saveAs(blob, nombreArchivo);
+
+  }
+
+
+
+return (
+  <Container className="mt-5">
+    <br />
+    <h4>Gestión de Productos</h4>
+    <Button className="mb-3" onClick={() => setShowModal(true)}>
+      Agregar producto
+    </Button>
+
+    {/* Botón para PDF de detalle (reemplaza 'producto' con el producto seleccionado) */}
+    <Button
+      variant="outline-secondary"
+      size="sm"
+      className="me-2"
+      onClick={() => generarPDFDetalleProducto(productos)} // Asegúrate de definir 'producto'
+    >
+      <i className="bi bi-filetype-pdf"></i>
+    </Button>
+
+    <CuadroBusquedas
+      searchText={searchText}
+      handleSearchChange={handleSearchChange}
+    />
+
+    <TablaProductos
+      openEditModal={openEditModal}
+      openDeleteModal={openDeleteModal}
+      handleCopy={handleCopy}
+      openQRModal={openQRModal}
+      productos={paginatedProductos}
+      totalItems={productosFiltrados.length}
+      itemsPerPage={itemsPerPage}
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+    />
+
+    <ModalRegistroProducto
+      showModal={showModal}
+      setShowModal={setShowModal}
+      nuevoProducto={nuevoProducto}
+      handleInputChange={handleInputChange}
+      handleImageChange={handleImageChange}
+      handleAddProducto={handleAddProducto}
+      categorias={categorias}
+    />
+
+    <ModalEdicionProducto
+      showEditModal={showEditModal}
+      setShowEditModal={setShowEditModal}
+      productoEditado={productoEditado}
+      handleEditInputChange={handleEditInputChange}
+      handleEditImageChange={handleEditImageChange}
+      handleEditProducto={handleEditProducto}
+      categorias={categorias}
+    />
+
+    <ModalEliminacionProducto
+      showDeleteModal={showDeleteModal}
+      setShowDeleteModal={setShowDeleteModal}
+      handleDeleteProducto={handleDeleteProducto}
+    />
+
+    <ModalQR
       show={showQRModal}
       handleClose={handleCloseQRModal}
       qrUrl={selectedUrl}
-       />
-    </Container>
-  );
+    />
+
+    <Col lg={3} md={4} sm={4} xs={5}>
+      <Button
+        className="mb-3"
+        onClick={generarPDFProductos}
+        variant="secondary"
+        style={{ width: "100%" }}
+      >
+        Generar reporte PDF
+      </Button>
+    </Col>
+
+    <Col lg={3} md={4} sm={4} xs={5}>
+      <Button
+        className="mb-3"
+        onClick={exportarExcelProductos}
+        variant="secondary"
+        style={{ width: "100%" }}
+      >
+        Generar Excel
+      </Button>
+    </Col>
+  </Container>
+);
 };
 
 export default Productos;
